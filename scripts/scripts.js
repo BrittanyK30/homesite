27b11f5098e544c539a75173f79519e586b3f851
BrittanyK30 <brittanythirtyacre@gmail.com>
2019-03-26 13:48:01 -0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+function myMap() {
+  //mapProp defines the properties of  the map
+  var mapProp = {
+    //center where to center the map in  regard to lat and long, zoom specifies the zoom level for the map
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  //var map var creates a new map inside the div elm w google map using the params that are passed mapProp
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+/* add a marker with the Marker constructor to create the new marker(((( google.maps.Marker({position: mustBeSetToSomething}))))) */
+var marker = new google.maps.Marker({
+  position: myCenter,
+  animation: google,maps.Animation.BOUNCE
+});
+
+//add the marker to the map using setMap() method
+marker.setMap(map);
